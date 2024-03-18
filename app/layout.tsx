5ca0cff96e3feb95c1d1b34201e3fd4c612fec6d
mitama <loligothick@gmail.com>
2024-03-18 16:14:04 +0900

    feat: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import React from "react";
 import { Provider } from "jotai";
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className={inter.className}>
         <Provider>{children}</Provider>
+        <Analytics />
       </body>
       <GoogleTagManager gtmId={`${process.env.GTAG}`} />
       <GoogleAnalytics gaId={`${process.env.GTAG}`} />
