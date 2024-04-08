b6ef15b2b028d5ea43b23e01f44157e74195140d
mitama <loligothick@gmail.com>
2024-04-08 17:16:33 +0900

    refactor: JSDoc

--- component/sortable/Sortable.tsx
+++ component/sortable/Sortable.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, SetStateAction, useState } from 'react';
+import React, { PropsWithChildren, SetStateAction } from 'react';
 
 import {
   closestCenter,
@@ -18,19 +18,74 @@ import {
   SortingStrategy,
 } from '@dnd-kit/sortable';
 
-export type SortableListProps<T extends { id: UniqueIdentifier }> = {
+/**
+ * Props for Sortable component
+ */
+export type SortableProps<T extends { id: UniqueIdentifier }> = {
+  /**
+   * ソート対象のアイテム
+   */
   items: T[];
-  setItems?: React.Dispatch<SetStateAction<T[]>>;
+  /**
+   * ソート対象のアイテムを更新する {@link React.Dispatch} 関数
+   *
+   * @example
+   * const [items, setItems] = useState<T[]>([...]);
+   * <Sortable items={items} setItems={setItems}>
+   *   ...
+   * </Sortable>
+   */
+  setItems: React.Dispatch<SetStateAction<T[]>>;
+  /**
+   * ソートの戦略
+   *
+   * @see https://docs.dndkit.com/presets/sortable/sortable-context#strategy
+   */
   strategy?: SortingStrategy;
+  /**
+   * DndContext のプロパティ
+   * {@link DndContextProps} をカスタマイズしたい場合に使用する
+   *
+   * @example
+   * const dnd = {
+   *  autoScroll: true,
+   * };
+   * <Sortable dnd={dnd}>
+   *   ...
+   * </Sortable>
+   *
+   * @see https://docs.dndkit.com/api-documentation/context-provider#props
+   */
   dnd?: DndContextProps;
 };
 
-export default function Sortable<T extends { id: UniqueIdentifier }>(
-  props: PropsWithChildren<SortableListProps<T>>,
-) {
-  const [items, setItems] = props.setItems
-    ? [props.items, props.setItems]
-    : useState(props.items);
+/**
+ * Sortable component
+ * @param items - ソート対象のアイテム
+ * @param setItems - ソート対象のアイテムを更新する {@link React.Dispatch} 関数
+ * @param children - ソート対象のアイテムを表示するコンポーネント
+ * @param strategy - [optional] ソートの戦略
+ * @param dnd - [optional] {@link DndContext} のプロパティ
+ * @return React.ReactNode
+ * @remarks
+ *  - ソート対象のアイテムはコンポーネントの外側で管理することが前提
+ * @example
+ * const [items, setItems] = useState<T[]>([...]);
+ * <Sortable items={items} setItems={setItems}>
+ *   {items.map((item) => (
+ *     <SortableItem key={item.id} id={item.id}>
+ *       {item.name}
+ *     </SortableItem>
+ *   ))}
+ * </Sortable>
+ */
+export default function Sortable<T extends { id: UniqueIdentifier }>({
+  items,
+  setItems,
+  children,
+  strategy,
+  dnd,
+}: PropsWithChildren<SortableProps<T>>) {
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -42,11 +97,11 @@ export default function Sortable<T extends { id: UniqueIdentifier }>(
     <DndContext
       sensors={sensors}
       collisionDetection={closestCenter}
-      onDragEnd={props.dnd?.onDragEnd ? props.dnd?.onDragEnd : handleDragEnd}
-      {...props.dnd}
+      onDragEnd={dnd?.onDragEnd ? dnd?.onDragEnd : handleDragEnd}
+      {...dnd}
     >
-      <SortableContext items={items} strategy={props.strategy}>
-        {props.children}
+      <SortableContext items={items} strategy={strategy}>
+        {children}
       </SortableContext>
     </DndContext>
   );
