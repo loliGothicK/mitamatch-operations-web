d5a1fb22cdd7a444f4489ae0fe880509887d5959
mitama <loligothick@gmail.com>
2024-04-19 03:06:57 +0900

    feat: Sentry

---
+++ app/global-error.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { captureException } from '@sentry/nextjs';
+import { default as NextError } from 'next/error';
+import { useEffect } from 'react';
+
+export default function GlobalError({ error }) {
+  useEffect(() => {
+    captureException(error);
+  }, [error]);
+
+  return (
+    <html lang={'en'}>
+      <body>
+        <NextError statusCode={404} />
+      </body>
+    </html>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,48 @@
+import { withSentryConfig } from '@sentry/nextjs';
 /** @types {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-    },
+  experimental: {
+    // biome-ignore lint/style/useNamingConvention: <explanation>
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
-export default nextConfig;
+export default withSentryConfig(
+  nextConfig,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    // biome-ignore lint/style/useNamingConvention: <explanation>
+    transpileClientSDK: true,
+
+    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
+);
