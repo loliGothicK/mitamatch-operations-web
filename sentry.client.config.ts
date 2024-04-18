bbd6c3c3bc898bd9b9ee43871496803bed6b28d3
mitama <loligothick@gmail.com>
2024-04-19 05:10:09 +0900

    feat: profiling

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -23,6 +23,8 @@ init({
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: 1.0,
 
+  profilesSampleRate: 1.0,
+
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
     new BrowserProfilingIntegration(),
