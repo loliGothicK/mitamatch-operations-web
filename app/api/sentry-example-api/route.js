82703013e19a87e8bb37724db4ba976a5a145e56
mitama <loligothick@gmail.com>
2024-04-19 04:01:49 +0900

    chore: delete example

--- app/api/sentry-example-api/route.js
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic';
-
-// A faulty API route to test Sentry's error monitoring
-// biome-ignore lint/style/useNamingConvention: <explanation>
-export function GET() {
-  throw new Error('Sentry Example API Error');
-  // return NextResponse.json({ data: 'Testing Sentry Error...' });
-}
