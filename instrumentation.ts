15832ecf6e53019236c47629dd4eeacff382d379
mitama <loligothick@gmail.com>
2024-11-08 00:20:30 +0900

    feat: remove unused deps

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,12 @@
+import {registerOTel} from "@vercel/otel";
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./instrumentation.node.js');
+  }
+  else {
+    registerOTel({
+      serviceName: "mitamatch-operations",
+    });
   }
 }
