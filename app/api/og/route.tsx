f8f3ef58a91948c1d5bae59b51753f0dee63783c
mitama <loligothick@gmail.com>
2025-03-21 21:40:52 +0900

    feat(route): implement dynamic image generation for deck and timeline parameters

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,5 +1,6 @@
 import { ImageResponse } from 'next/og';
 import type { NextRequest } from 'next/server';
+import { match } from 'ts-pattern';
 
 export const runtime = 'edge';
 
@@ -8,66 +9,101 @@ const size = {
   height: 500,
 };
 
-export async function GET(reqest: NextRequest) {
-  const param = reqest.nextUrl.searchParams.get('deck');
-  if (param === null) {
-    return new Response('Deck not found', {
-      status: 500,
-    });
-  }
-  try {
-    const { legendary, deck } = (await fetch(
-      new URL(`/api/deck?deck=${param}`, reqest.nextUrl),
-    ).then(res => res.json())) as { legendary: string[]; deck: string[] };
+const style = {
+  background: '#8bd0dd',
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  flexWrap: 'wrap' as const,
+};
 
-    return new ImageResponse(
-      <div
-        style={{
-          background: '#8bd0dd',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          flexWrap: 'wrap',
-        }}
-      >
-        {legendary.map(name => {
-          return (
-            <img
-              src={`https://github.com/loliGothicK/mitamatch-operations-web/raw/main/public/memoria/${name}.png`}
-              key={name}
-              alt={'memoria'}
-              width={100}
-              height={100}
-            />
+export async function GET(reqest: NextRequest) {
+  const params = reqest.nextUrl.searchParams;
+  try {
+    return match(params)
+      .when(
+        params => params.has('deck'),
+        async params => {
+          const param = params.get('deck') as string;
+          const { legendary, deck } = (await fetch(
+            new URL(`/api/deck?deck=${param}`, reqest.nextUrl),
+          ).then(res => res.json())) as { legendary: string[]; deck: string[] };
+
+          return new ImageResponse(
+            <div style={style}>
+              {legendary.map(name => {
+                return (
+                  <img
+                    src={`https://github.com/loliGothicK/mitamatch-operations-web/raw/main/public/memoria/${name}.png`}
+                    key={name}
+                    alt={'memoria'}
+                    width={100}
+                    height={100}
+                  />
+                );
+              })}
+              <div
+                key={'divider'}
+                style={{
+                  gridColumn: '1 / -1',
+                  height: '2px',
+                  background: '#000',
+                  margin: '10px 0',
+                }}
+              />
+              {deck.map(name => {
+                return (
+                  <img
+                    src={`https://github.com/loliGothicK/mitamatch-operations-web/raw/main/public/memoria/${name}.png`}
+                    key={name}
+                    alt={'memoria'}
+                    width={100}
+                    height={100}
+                  />
+                );
+              })}
+            </div>,
+            {
+              ...size,
+            },
           );
-        })}
-        <div
-          key={'divider'}
-          style={{
-            gridColumn: '1 / -1',
-            height: '2px',
-            background: '#000',
-            margin: '10px 0',
-          }}
-        />
-        {deck.map(name => {
-          return (
-            <img
-              src={`https://github.com/loliGothicK/mitamatch-operations-web/raw/main/public/memoria/${name}.png`}
-              key={name}
-              alt={'memoria'}
-              width={100}
-              height={100}
-            />
+        },
+      )
+      .when(
+        params => params.has('timeline'),
+        async params => {
+          const param = params.get('timeline') as string;
+          const { timeline } = (await fetch(
+            new URL(`/api/timeline?timeline=${param}`, reqest.nextUrl),
+          ).then(res => res.json())) as { timeline: string[] };
+
+          return new ImageResponse(
+            <div style={style}>
+              {timeline.map(name => (
+                <img
+                  src={`https://github.com/loliGothicK/mitamatch-operations-web/raw/main/public/order/${name}.png`}
+                  key={name}
+                  alt={'memoria'}
+                  width={100}
+                  height={100}
+                />
+              ))}
+            </div>,
+            {
+              width: 1200,
+              height: 100,
+            },
           );
-        })}
-      </div>,
-      {
-        ...size,
-      },
-    );
+        },
+      )
+      .otherwise(
+        async () =>
+          new Response('param (deck or timeline) not found', {
+            status: 500,
+          }),
+      );
   } catch (_) {
     return new Response('Failed to generate the image', {
       status: 500,
---
+++ app/api/timeline/route.ts
@@ -0,0 +1,17 @@
+'use server';
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { restore } from '@/actions/restore';
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const param = searchParams.get('deck');
+  if (param === null) {
+    return NextResponse.json({});
+  }
+  const timeline = await restore({ target: 'timeline', param });
+  return NextResponse.json({
+    timeline: timeline.map(item => item.name),
+  });
+}
